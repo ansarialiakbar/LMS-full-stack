27a3231f25f8b207d7acf0de6bbd73b4800395a6
Ali Akbar Ansari <142114003+ansarialiakbar@users.noreply.github.com>
2025-01-06 16:25:51 +0530

    Update axiosInstance.js

--- lms-frontend/src/Helpers/axiosInstance.js
+++ lms-frontend/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://lms-backend-j6l4.onrender.com";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
