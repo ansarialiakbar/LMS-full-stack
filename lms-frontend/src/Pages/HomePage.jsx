8b9b0581f9368e32eaff265532186f7d80a4bff7
Ali Akbar Ansari <142114003+ansarialiakbar@users.noreply.github.com>
2025-01-07 20:29:35 +0530

    Update HomePage.jsx

--- lms-frontend/src/Pages/HomePage.jsx
+++ lms-frontend/src/Pages/HomePage.jsx
@@ -1,62 +1,45 @@
-import { Link } from 'react-router-dom';
-import HomeLayout from '../Layouts/HomeLayout'
-<<<<<<< HEAD
-const homePageMainImage = '/Assets/Images/homePageMainImage.png';
+import { Link } from "react-router-dom";
 
+const homePageMainImage = "/Assets/Images/homePageMainImage.png";
+import HomeLayout from "../Layouts/HomeLayout";
 
-=======
-
-const homePageMainImage = '/Assets/Images/homePageMainImage.png';;
-
-
-
->>>>>>> c2db5338c16182f68b77e54a6c1ba290b7a25cde
-function HomePage(){
-    return(
+function HomePage() {
+    return (
         <HomeLayout>
             <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
-               <div className="w-1/2 space-y-6">
-               <h1 className='text-5xl font-semibold'>
-                  Find out best 
-                  <span className="text-yellow-500 font-bold">
-                    Online Courses
-                  </span>
-               </h1>
-               <p className='text-xl text-gray-200'>
-                we have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost
-               </p>
-               <div className="space-x-6">
-                <Link to= "/courses">
-                <button className='bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300'>
-                    Explore Courses
-                </button>
-                </Link>
+                <div className="w-1/2 space-y-6">
+                    <h1 className="text-5xl font-semibold">
+                        Find out best
+                        <span className="text-yellow-500 font-bold">
+                            Online Courses
+                        </span>
+                    </h1>
+                    <p className="text-xl text-gray-200">
+                        We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
+                    </p>
 
-                <Link to= "/contact">
-                <button className=' bg-slate-400 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-700 transition-all ease-in-out duration-300'>
-                    Contact Us
-                </button>
-                </Link>
-               </div>
-               </div>
+                    <div className="space-x-6">
+                        <Link to="/courses">
+                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Explore courses
+                            </button>
+                        </Link>
 
-               <div className='w-1/2 flex items-center justify-center'>
-<<<<<<< HEAD
-               <img alt='homepage' src={ homePageMainImage} />
-=======
+                        <Link to="/contact">
+                            <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                                Contact Us
+                            </button>
+                        </Link>
+                    </div>
+                </div>
 
-               <img alt='homepage' src={ homePageMainImage} />
+                <div className="w-1/2 flex items-center justify-center">
+                    <img alt="homepage image" src={homePageMainImage} />
+                </div>
 
-              
-
->>>>>>> c2db5338c16182f68b77e54a6c1ba290b7a25cde
-           
-               </div>
             </div>
-
         </HomeLayout>
-
     );
-
 }
+
 export default HomePage;
