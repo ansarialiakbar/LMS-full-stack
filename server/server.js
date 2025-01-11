1f33d41c3ce6af7df71f66352ff0e33d69be617b
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-01-11 18:41:25 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -21,11 +21,15 @@ connectionToDB().then(() => {
 });
 
 // Check if we're running locally (not in serverless)
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const PORT = process.env.PORT || 5000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+//   });
+// }
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
